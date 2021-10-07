ab405d9717b1a9141d611f2cfd350eadb58829dc
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-07 19:52:25 +0530

    create cricinfoextractor

---
+++ web scrapping /CricinfoExtractor.js
@@ -0,0 +1,29 @@
+// The purpose of this project is to extract information of worldcup 2019 from cricinfo.
+// that in the form of excel and pdf scorecard
+// the real purpose is to learn how to extract information and get experience with js
+// a very good reason to ever make a project is to have fun
+
+const minimist = require("minimist");
+
+// npm init -y
+// npm install minimist
+// npm install axios
+// npm install jsdom
+// npm install excel4node
+// npm install pdf-lib
+
+// node CricinfoExtractor.js --excel=worldcup.csv --dataFolder=data --source="https://www.espncricinfo.com/series/ipl-2021-1249214/match-results" 
+
+let minimist = require("minimist");
+let axios = require("axios");
+let jsdom = require("jsdom");
+let excel4node = require("excel4node");
+let pdf = require("pdf-lib");
+const minimist = require("minimist");
+
+let args = minimist(process.argv);
+console.log(args.source);
+console.log(args.excel);
+console.log(args.dataFolder);
+
+
